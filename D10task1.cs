6ebb1f5d5881bb35425524d8bb423e203e344a0c
Abdulalsudais <91174429+Abdulalsudais@users.noreply.github.com>
2022-03-21 16:50:13 +0530

    Add files via upload

---
+++ D10task1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+
+namespace ConsoleApp2
+{
+    class D10Task1
+    {
+        public static void Main()
+        {
+
+            FileStream fs = new FileStream("D:\\Softura\\training day 10\\D10Task1.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter bw = new StreamWriter(fs);
+            List<string> str = new List<String>();
+            str.Add("Lassi");
+            str.Add("Malai");
+            str.Add("CHeese cake");
+            str.Add("Cup Cake");
+            str.Add("Jell-O");
+            foreach (string d in str)
+            {
+                bw.Write("{0}\n" ,d);
+            }
+            bw.Flush();
+            bw.Close();
+            fs.Close();
+            FileStream fs1 = new FileStream("D:\\Softura\\training day 10\\D10Task1.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamReader sr = new StreamReader(fs1);
+            String s = sr.ReadToEnd();
+            sr.Close();
+            fs1.Close();
+            Console.WriteLine(s);
+            FileInfo fi = new FileInfo(@"D:\\Softura\\training day 10\\D10Task1.txt");
+            Console.WriteLine(fi.Length);
+            Console.WriteLine(fi.CreationTime);
+            Console.Read();
+        }
+    }
+}
---
+++ D10task2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    class D10task2
+    {
+        public static void Main()
+
+        {
+            FileStream fs = new FileStream ("D:\\Softura\\training day 10\\D10task2.txt",FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryWriter bw = new BinaryWriter(fs);
+            bw.Write("56846\n");
+            bw.Write("Abdul\n");
+            bw.Write("Male\n");
+            bw.Write("22");
+            bw.Flush();
+            fs.Close();
+            FileStream fs1 = new FileStream(@"D:\\Softura\\training day 10\\D10task2.txt", FileMode.Open, FileAccess.ReadWrite);
+            BinaryReader br = new BinaryReader(fs1);
+            BinaryReader br1 = new BinaryReader(fs1);
+            BinaryReader br2 = new BinaryReader(fs1);
+            BinaryReader br3 = new BinaryReader(fs1);
+            string str = br.ReadString();
+            string str1 = br1.ReadString();
+            string str2 = br2.ReadString();
+            string str3 = br3.ReadString();
+            Console.WriteLine(str);
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+            Console.WriteLine(str3);
+            fs1.Close();
+            Console.Read();
+        }
+
+
+    }
+}
+    
+
